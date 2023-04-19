d1108afeb7bc04e2653861cd493c8587384a7be3
LFernandoPortugal <falconbladex1@gmail.com>
2023-04-18 21:16:43 -0500

    Finalizado sin redes sociales

--- js/scripts_index.js
+++ js/scripts_index.js
@@ -1,26 +1,96 @@
 // API para noticias
-// Hacer una solicitud a News API
-const apiKey = '67a5d580af694c38a553d08afb2b2c9e'; // Reemplaza esto con tu API key
-const url = `https://newsapi.org/v2/top-headlines?country=mx&category=technology&apiKey=${apiKey}`;
+let cantidadNoticias = 5;
+let pageFinal = cantidadNoticias;
+let pageInicial = 0;
+let temaActual = "Tecnología";
 
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    // Mostrar los resultados en una sección específica de tu HTML
-    const noticias = data.articles;
-    let noticiasHtml = '';
+let noticias = {
+    "apiKey":"67a5d580af694c38a553d08afb2b2c9e",
+    fetchNoticias:function(categoria){
+        fetch(
+            "https://newsapi.org/v2/everything?q="
+            +categoria+
+            "&languaje=es&apiKey="+this.apiKey
+        )
+        .then((response)=>response.json())
+        .then((data)=>this.displayNoticias(data));
+    },
+    displayNoticias: function(data){
+        //elimino todo si ha seleccionado un nuevo tema
+        if(pageInicial==0){
+            document.querySelector(".container-noticias").textContent ="";
+        }
 
-    for (let i = 0; i < noticias.length; i++) {
-      const imagen = noticias[i].urlToImage ? noticias[i].urlToImage : 'https://via.placeholder.com/150x150.png?text=No+image+available';
-      noticiasHtml += `<div class="noticia">`;
-      noticiasHtml += `<img src="${imagen}" alt="${noticias[i].title}">`;
-      noticiasHtml += `<h2>${noticias[i].title}</h2>`;
-      noticiasHtml += `<p>${noticias[i].description}</p>`;
-      noticiasHtml += `<a href="${noticias[i].url}" target="_blank">Leer más</a>`;
-      noticiasHtml += `</div>`;
+
+        for(i=pageInicial;i<=pageFinal;i++){
+            const {title} = data.articles[i];
+            let h2 = document.createElement("h2");
+            h2.textContent = title;
+    
+            const {urlToImage} = data.articles[i];
+            let img = document.createElement("img");
+            img.setAttribute("src", urlToImage);
+
+            let info_item = document.createElement("div");
+            info_item.className = "info_item";
+            const {publishedAt} = data.articles[i];
+            let fecha = document.createElement("span");
+            let date = publishedAt;
+            date=date.split("T")[0].split("-").reverse().join("-");
+            fecha.className = "fecha";
+            fecha.textContent = date;
+
+            const {name} = data.articles[i].source;
+            let fuente = document.createElement("span");
+            fuente.className = "fuente";
+            fuente.textContent = name;
+
+            info_item.appendChild(fecha);
+            info_item.appendChild(fuente);
+
+            const {url} = data.articles[i];
+
+            let item = document.createElement("div");
+            item.className = "item";
+            item.appendChild(h2);
+            item.appendChild(img);
+            item.appendChild(info_item);
+            // item.setAttribute("onclick", "location.href='"+url+"'");
+            // con esta linea modificada se puede abrir en nueva pestaña
+            item.setAttribute("onclick", "window.open('" + url + "', '_blank')");
+            document.querySelector(".container-noticias").appendChild(item);
+        }
+
+        let btnSiguiente = document.createElement("span");
+        btnSiguiente.id = "btnSiguiente";
+        btnSiguiente.textContent = "Ver más";
+        btnSiguiente.setAttribute("onclick","siguiente()");
+        document.querySelector(".container-noticias").appendChild(btnSiguiente);
     }
+}
+function buscar(cat){
+    pageInicial = 0;
+    pageFinal = cantidadNoticias;
+    temaActual = cat;
+    noticias.fetchNoticias(cat);
+}
 
-    const noticiasContainer = document.querySelector('#noticias-tecnologia');
-    noticiasContainer.innerHTML = noticiasHtml;
-  })
-  .catch(error => console.log(error));
+function buscarTema(){
+    pageInicial = 0;
+    pageFinal = cantidadNoticias;
+
+    let tema = document.querySelector("#busqueda").value;
+    temaActual = tema;
+    noticias.fetchNoticias(temaActual);
+}
+
+function siguiente(){
+    pageInicial = pageFinal + 1;
+    pageFinal = pageFinal + cantidadNoticias + 1;
+    //eliminamos el botón siguiente
+    document.querySelector("#btnSiguiente").remove();
+    noticias.fetchNoticias(temaActual);
+
+}
+
+noticias.fetchNoticias(temaActual);
